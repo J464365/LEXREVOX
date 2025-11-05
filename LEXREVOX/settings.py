7bf48db66f7ee78756e426b417f22013b640699e
Jhon <you@example.comC:/Users/Jhon/AppData/Local/Programs/Python/Python312/python.exe>
2025-11-04 21:05:28 -0500

    xd

--- LEXREVOX/settings.py
+++ LEXREVOX/settings.py
@@ -135,3 +135,6 @@ STORAGES = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
