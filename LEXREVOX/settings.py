eef1e252f65087836ab69364244eb0a6d42c82e9
Jhon <you@example.comC:/Users/Jhon/AppData/Local/Programs/Python/Python312/python.exe>
2025-11-06 16:15:34 -0500

    py

--- LEXREVOX/settings.py
+++ LEXREVOX/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-nzm0=u5!-w-r$1ep)a(g7^lmg0^icrz96mu*&c#=2ioq3$hyb9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['165.227.62.39', 'www.lexrevox.com.co', 'lexrevox.com.co']
 
 
 # Application definition
@@ -135,6 +135,3 @@ STORAGES = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
